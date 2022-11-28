7a507fe1cfe8ac0f3983e2795dc7638829295bec
Matthew <matthewduff0912@gmail.com>
2022-11-28 20:36:57 +0000

    Updated plugins

--- TruckService_Backend/build.gradle.kts
+++ TruckService_Backend/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     application
     kotlin("jvm") version "1.7.22"
     id("io.ktor.plugin") version "2.1.3"
+                id("org.jetbrains.kotlin.plugin.serialization") version "1.7.22"
 }
 
 group = "lupinespace.com"
@@ -22,7 +23,11 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
--- TruckService_Backend/src/test/kotlin/lupinespace/com/ApplicationTest.kt
+++ TruckService_Backend/src/test/kotlin/lupinespace/com/ApplicationTest.kt
@@ -2,6 +2,14 @@ package lupinespace.com
 
 import io.ktor.server.routing.*
 import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.auth.*
+import io.ktor.util.*
+import io.ktor.server.auth.jwt.*
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
