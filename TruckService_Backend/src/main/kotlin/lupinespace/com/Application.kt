196d9b2fbaa6c4d184e77ec64b384c4345c40247
Matthew <matthewduff0912@gmail.com>
2022-11-29 10:48:21 +0000

    Added MongoDB and setup MongoDB connection

--- TruckService_Backend/src/main/kotlin/lupinespace/com/Application.kt
+++ TruckService_Backend/src/main/kotlin/lupinespace/com/Application.kt
@@ -1,11 +1,22 @@
 package lupinespace.com
 
+import com.mongodb.MongoClient
+import com.mongodb.MongoException
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import lupinespace.com.plugins.*
 
 fun main() {
+    var mongoClient: MongoClient? = null
+    try {
+        mongoClient = MongoClient(String.format("mongodb+srv://lupines-space:%s@socialites.fezg0.mongodb.net/?retryWrites=true&w=majority", System.getenv("MONGO_PW")))
+        println("Application connected to MongoDB")
+    } catch (e: MongoException) {
+        e.printStackTrace()
+    } finally {
+        mongoClient!!.close()
+    }
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
