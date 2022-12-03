f38f0331d2796e5a4251b4f105b7ce2980273e6f
Matthew <matthewduff0912@gmail.com>
2022-12-03 01:13:52 +0000

    Added Bson ID to truck document

--- TruckService_Backend/src/main/kotlin/lupinespace/com/data/models/Truck.kt
+++ TruckService_Backend/src/main/kotlin/lupinespace/com/data/models/Truck.kt
@@ -1,9 +1,13 @@
 package lupinespace.com.data.models
 
 import kotlinx.serialization.Serializable
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
 
 @Serializable
 data class Truck(
+    @BsonId
+    var id: String = ObjectId().toString(),
     val vrm: String,
     val model: String
 )
